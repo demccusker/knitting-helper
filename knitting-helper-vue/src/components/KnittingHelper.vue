b9a362140070610e11b308f25e498f4820ca7c74
Darcy <110570447+demccusker@users.noreply.github.com>
2024-12-16 16:36:07 -0800

    Working on converting rows to complete a pattern

--- knitting-helper-vue/src/components/KnittingHelper.vue
+++ knitting-helper-vue/src/components/KnittingHelper.vue
@@ -2,19 +2,70 @@
     <div>
         <form action="">
             <label for="pattern">Add your pattern here</label>
-            <textarea name="pattern" rows="10" cols="30" v-model="pattern"> 
+            <textarea name="pattern" rows="10" cols="30" v-model="pattern">
                 </textarea>
-<div>{{ pattern }}</div>
+            <div class="labeled-pattern">
+                <div v-for="(row, index) in labeledPattern" :key="index">
+                    {{ row }}
+                </div>
+            </div>
         </form>
     </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      pattern: '' 
-    };
-  }
+    data() {
+        return {
+            pattern: '',
+            labeledPattern: []
+        };
+    },
+    watch: {
+        pattern() {
+            this.addLabels();
+        }
+    },
+    methods: {
+
+        addLabels() {
+            const lines = this.pattern.split('\n');
+            let cleanedLines = [];
+            let knitAllKnitsFound = false;
+            for (let i = 0; i < lines.length; i++) {
+                let line = lines[i];
+                let cleanLine = '';
+                if (!(/^row\s+\d+:/i.test(line))){
+                    cleanLine = `Row ${i + 1}: ` + line;
+
+                
+                }
+                if (/knit (the )?knits and purl (the )?purls/i.test(line)) {
+                    knitAllKnitsFound = true;
+                    let previousLine = cleanedLines[i - 1] 
+                    cleanLine = this.convertRow(previousLine);
+                }
+                else if (knitAllKnitsFound & i % 2 == 0){
+                    let previousLine = cleanedLines[i - 1] 
+                    cleanLine = this.convertRow(previousLine);
+
+                }
+                cleanedLines.push(cleanLine);
+            }
+            return cleanedLines;
+        },
+        convertRow(row) {
+            console.log("calling function")
+            const index = row.index;
+            let aboveRow = this.labeledPattern[index - 1];
+            let spacedRow = aboveRow.split(' ');
+            console.log(aboveRow);
+
+
+        }
+
+
+
+    }
 };
 </script>
